f1689b2d65c84209bbf2d80bd62c3f44b1280a79
Olie 3 <therealolie@proton.me>
2024-01-16 21:00:32 +0100

    meow

--- init.sh
+++ init.sh
@@ -8,9 +8,8 @@ fi
 mkdir ~/.config -p
 cp -r ./config/* ~/.config
 if [ "${PLATFORM}" = PC ]; then
-	cp -r ./i3 ~/.config
-	sudo cp ./misc/pl /usr/share/X11/xkb/symbols/pl
-	sudo cp ./misc/sudoers /etc/sudoers
+	sudo cmp -s ./misc/pl /usr/share/X11/xkb/symbols/pl || sudo cp ./misc/pl /usr/share/X11/xkb/symbols/pl
+	sudo cmp -s ./misc/sudoers /etc/sudoers || sudo cp ./misc/sudoers /etc/sudoers
 fi
 rm -f ~/.bashrc
 ln -s ~/.config/shell/profile ~/.bashrc
